ed8b9b89b44126106e09a99ea8181f538880468d
Fernando Serpa <serpaef@outlook.com>
2021-08-24 11:47:16 -0300

    Hello World em React

---
+++ src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Component extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <p>Olá! Me chamo Fernando e esse é meu primeiro passo em React.</p>
+      </div>
+    )
+  }
+}
+
+export default Component;
